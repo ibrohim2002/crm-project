7b376c2cf646ef43ff35aeb98d93a1619dd92b24
ibrohim2002 <74803608+ibrohim2002@users.noreply.github.com>
2025-03-11 02:40:53 +0500

    Update index.jsx

--- src/components/Orders/OrderList/index.jsx
+++ src/components/Orders/OrderList/index.jsx
@@ -6,7 +6,7 @@ import { ModalWindow } from "./modal";
 
 export const OrderList = () => {
   // const url = import.meta.env.VITE_BASE_URL;
-  const url = 'https://sheet.best/api/sheets/08c05757-65c7-4e6a-a744-36ad0714d2a1';
+  const url = 'https://api.sheetbest.com/sheets/b9919168-806d-4ce7-b4fc-30667c9c0808';
   const [orders, setOrders] = useState([]);
   const [modal, setModal] = useState(false);
   const [data, setData] = useState({});
